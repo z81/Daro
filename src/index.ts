1a613069544804cf97effd079c7335908fcd5cbb
Иклюков Александр Александрович <muzo0@yandex.ru>
2021-03-06 14:37:53 +0300

    trace

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { F } from "./core/f";
+import { F, enableTrace } from "./core/f";
 import { delay } from "./core/fn";
 import { pipe } from "./core/pipe";
 import { Queue } from "./core/queue";
@@ -33,18 +33,20 @@ setInterval(() => {
 
 const ds = pipe(
   F.access<{ queue: Queue<string> }>(),
-  F.map(async function* (_, ctx) {
+  F.map(async function* generateRandString(_, ctx) {
     let i = 0;
     while (true) {
       await delay(500);
       yield `str ${i++} ${Math.random()}`;
 
-      // if (i >= 2) {
-      return;
-      // }
+      if (i >= 5) {
+        return;
+      }
     }
   }),
-  F.map((msg, ctx) => ctx.queue.add(msg)),
+  F.map(function queueAdd(msg, ctx) {
+    return ctx.queue.add(msg);
+  }),
   F.provide({ queue: new Queue<string>() }),
   F.module((_, ctx) => ({
     resolve: { queueName: "queueName 1" },
@@ -56,12 +58,12 @@ pipe(
   F.access<{
     ds: typeof ds;
   }>(),
-  F.map(function* (_, ctx) {
+  F.map(function* makeQueueName(_, ctx) {
     for (const workerId of [1]) {
       yield `queue: ${ctx.ds.resolve.queueName} / workerId: ${workerId}`;
     }
   }),
-  F.map(async function* (prefix, ctx) {
+  F.map(async function* getQueueValue(prefix, ctx) {
     for await (const ds of ctx.queue) {
       yield `${prefix} / value: ${ds}`;
     }
@@ -73,68 +75,3 @@ pipe(
     v.then((v) => console.log("end", v));
   }
 );
-
-/*
-                        1897
-              6609[e]         4943   9299    6638
-6454 7254 9061 411 7998     9061      3706    6942
-*/
-
-/*
-ret 1897 6609 6454 undefined   ended
-ret 1897 6609 7254 undefined   ended
-result: queue: queueName
-ret 1897 4943 9061 queue: queueName
-ret 1897 6609 411 undefined     ended
-result: queue: queueName
-ret 1897 9299 3706 queue: queueName
-ret 1897 6609 7998 undefined    ended
-ret 1897 6609 undefined
-result: queue: queueName
-ret 1897 6638 6942 queue: queueName
-*/
-
-/*
-ret 180 5693 9502 undefined
-ret 180 5693 undefined
-
-      180
-  5693
-9502
-*/
-
-/*
-start 9808
-  start 9808 1191
-    value 9808 1191
-    value 9808 1191
-    isGenerator 9808 1191
-    for await start 9808 1191
-    for ster 9808 1191 str 0 0.9506798512147052
-    start 9808 1191 9179
-      make promise 9808 1191 1
-      for await end 9808 1191
-      wait genPromise 9808 1191 Promise { <pending> }
-      value 9808 1191 9179
-      value 9808 1191 9179
-      ret 9808 1191 9179 undefined
-      ?????
-    end 9808 1191 9179
-    rrr 9808 1191 undefined
-    ret 9808 1191 undefined
-  end 9808 1191
-  value 9808
-  value 9808
-  isGenerator 9808
-  for await start 9808
-  for ster 9808 queue: queueName 1 / workerId: 1
-  start 9808 374
-    make promise 9808 1 function (_) { return _; }
-    for await end 9808
-    wait genPromise 9808 Promise { <pending> }
-    value 9808 374
-    isGenerator 9808 374
-    for await start 9808 374
-    ?????
-  ?????
-*/
